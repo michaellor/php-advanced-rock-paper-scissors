cf89dc3c0c78a12f9b2f6c8a2ba280e522f2a624
Joe Karasek <joseph.karasek@gmail.com>
2016-03-07 22:48:52 -0800

    json to php object mid-step

---
+++ sample-data.php
@@ -0,0 +1,184 @@
+<?php
+$data = array (
+  'cols' =>
+  array (
+    0 =>
+    array (
+      'id' => '',
+      'label' => '',
+      'pattern' => '',
+      'type' => 'string',
+    ),
+    1 =>
+    array (
+      'id' => '',
+      'label' => 'Framework',
+      'pattern' => '',
+      'type' => 'number',
+    ),
+  ),
+  'rows' =>
+  array (
+    0 =>
+    array (
+      'c' =>
+      array (
+        0 =>
+        array (
+          'v' => 'Laravel',
+          'f' => NULL,
+        ),
+        1 =>
+        array (
+          'v' => 2112,
+          'f' => NULL,
+        ),
+      ),
+    ),
+    1 =>
+    array (
+      'c' =>
+      array (
+        0 =>
+        array (
+          'v' => 'Symfony2',
+          'f' => NULL,
+        ),
+        1 =>
+        array (
+          'v' => 1005,
+          'f' => NULL,
+        ),
+      ),
+    ),
+    2 =>
+    array (
+      'c' =>
+      array (
+        0 =>
+        array (
+          'v' => 'Nette',
+          'f' => NULL,
+        ),
+        1 =>
+        array (
+          'v' => 703,
+          'f' => NULL,
+        ),
+      ),
+    ),
+    3 =>
+    array (
+      'c' =>
+      array (
+        0 =>
+        array (
+          'v' => 'Yii 2',
+          'f' => NULL,
+        ),
+        1 =>
+        array (
+          'v' => 620,
+          'f' => NULL,
+        ),
+      ),
+    ),
+    4 =>
+    array (
+      'c' =>
+      array (
+        0 =>
+        array (
+          'v' => 'CodeIgniter',
+          'f' => NULL,
+        ),
+        1 =>
+        array (
+          'v' => 482,
+          'f' => NULL,
+        ),
+      ),
+    ),
+    5 =>
+    array (
+      'c' =>
+      array (
+        0 =>
+        array (
+          'v' => 'PHPixie',
+          'f' => NULL,
+        ),
+        1 =>
+        array (
+          'v' => 420,
+          'f' => NULL,
+        ),
+      ),
+    ),
+    6 =>
+    array (
+      'c' =>
+      array (
+        0 =>
+        array (
+          'v' => 'Zend 2',
+          'f' => NULL,
+        ),
+        1 =>
+        array (
+          'v' => 346,
+          'f' => NULL,
+        ),
+      ),
+    ),
+    7 =>
+    array (
+      'c' =>
+      array (
+        0 =>
+        array (
+          'v' => 'No Framework',
+          'f' => NULL,
+        ),
+        1 =>
+        array (
+          'v' => 306,
+          'f' => NULL,
+        ),
+      ),
+    ),
+    8 =>
+    array (
+      'c' =>
+      array (
+        0 =>
+        array (
+          'v' => 'Yii 1',
+          'f' => NULL,
+        ),
+        1 =>
+        array (
+          'v' => 293,
+          'f' => NULL,
+        ),
+      ),
+    ),
+    9 =>
+    array (
+      'c' =>
+      array (
+        0 =>
+        array (
+          'v' => 'Phalcon',
+          'f' => NULL,
+        ),
+        1 =>
+        array (
+          'v' => 231,
+          'f' => NULL,
+        ),
+      ),
+    ),
+  ),
+);
+ ?>
