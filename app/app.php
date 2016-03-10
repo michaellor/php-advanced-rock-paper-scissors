ec5ec7fd57517263b2df84032d0bc80dd35bd98e
Joe Karasek <joseph.karasek@gmail.com>
2016-03-10 10:57:23 -0800

    add transparent piecharts for stats page

--- app/app.php
+++ app/app.php
@@ -227,34 +227,34 @@
         ));
     });
 
-    // $app->get("/play_pVc/{choice}", function($choice) use ($app){
-    //     $player_one_id = $_SESSION['player_one']['id'];
-    //     $player_one_choice = $choice;
-    //     $player_two_id = $_SESSION['player_two']['id'];
-    //     $player_two_choice = '';
-    //     $match_id = $_SESSION['match']['id'];
-    //     $new_game = new Game ($player_one_id, $player_one_choice, $player_two_id, $player_two_choice, null, null, $match_id);
-    //     $result = $new_game->playGame();
-    //
-    //     if ($new_game->getWinner() == $player_one_id)
-    //     {
-    //         $_SESSION['player_one']['score'] = $_SESSION['player_one']['score'] + 1;
-    //     }
-    //     elseif ($new_game->getWinner() == $player_two_id) {
-    //         $_SESSION['player_two']['score'] = $_SESSION['player_two']['score'] + 1;
-    //     }
-    //     return $app['twig']->render('game.html.twig', array(
-    //             'game' => 'Free Play',
-    //             'message'=> $result,
-    //             'navbar' => array(
-    //                     'userId' => $_SESSION['player_one']['id'],
-    //                     'userName' => $_SESSION['player_one']['name']
-    //             ),
-    //             'player1' => $_SESSION['player_one'],
-    //             'player2' => $_SESSION['player_two'],
-    //             'match'=> $_SESSION['match']
-    //     ));
-    // });
+    $app->get("/play_pVc/{choice}", function($choice) use ($app){
+        $player_one_id = $_SESSION['player_one']['id'];
+        $player_one_choice = $choice;
+        $player_two_id = $_SESSION['player_two']['id'];
+        $player_two_choice = '';
+        $match_id = $_SESSION['match']['id'];
+        $new_game = new Game ($player_one_id, $player_one_choice, $player_two_id, $player_two_choice, null, null, $match_id);
+        $result = $new_game->playGame();
+
+        if ($new_game->getWinner() == $player_one_id)
+        {
+            $_SESSION['player_one']['score'] = $_SESSION['player_one']['score'] + 1;
+        }
+        elseif ($new_game->getWinner() == $player_two_id) {
+            $_SESSION['player_two']['score'] = $_SESSION['player_two']['score'] + 1;
+        }
+        return $app['twig']->render('game.html.twig', array(
+                'game' => 'Free Play',
+                'message'=> $result,
+                'navbar' => array(
+                        'userId' => $_SESSION['player_one']['id'],
+                        'userName' => $_SESSION['player_one']['name']
+                ),
+                'player1' => $_SESSION['player_one'],
+                'player2' => $_SESSION['player_two'],
+                'match'=> $_SESSION['match']
+        ));
+    });
 
     //MATCH PLAY VERSUS COMPUTER
     $app->get("/pVc_match", function() use ($app){
